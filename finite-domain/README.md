f4d5c4ebbfc4156d90e4c0bf72ab62bcd545b4da
Nathanael <Shakadak@users.noreply.github.com>
2024-07-04 15:19:00 +0200

    saving work

---
+++ finite-domain/README.md
@@ -0,0 +1,5 @@
+# finite-domain
+
+trying to make 
+- https://overtond.blogspot.com/2008/07/haskell-sudoku-solver-using-finite.html
+work and compare it to Elixir
---
+++ finite-domain/app/Main.hs
@@ -0,0 +1,65 @@
+module Main (main) where
+
+import FD
+import Control.Monad
+import Sudoku
+
+main :: IO ()
+-- main = print runTest
+main = do
+    putStrLn "Start"
+    runSudoku
+
+runTest :: [[Int]]
+runTest = runFD test
+
+test :: FD s [Int]
+test = do
+    x <- newVar [0..3]
+    y <- newVar [0..3]
+    ( x `lessThan` y) `mplus` (x `same` y)
+    x `hasValue` 2
+    labelling [x, y]
+
+runSudoku :: IO ()
+runSudoku = printSudoku testSudoku_s15c
+
+
+testSudoku :: Puzzle
+testSudoku = [
+    0, 0, 0, 0, 8, 0, 0, 0, 0,
+    0, 0, 0, 1, 0, 6, 5, 0, 7,
+    4, 0, 2, 7, 0, 0, 0, 0, 0,
+    0, 8, 0, 3, 0, 0, 1, 0, 0,
+    0, 0, 3, 0, 0, 0, 8, 0, 0,
+    0, 0, 5, 0, 0, 9, 0, 7, 0,
+    0, 5, 0, 0, 0, 8, 0, 0, 6,
+    3, 0, 1, 2, 0, 4, 0, 0, 0,
+    0, 0, 6, 0, 1, 0, 0, 0, 0 ]
+
+testSudokuKindaMid :: Puzzle
+testSudokuKindaMid = [
+    2, 0, 6, 0, 0, 0, 0, 4, 9, 
+    0, 3, 7, 0, 0, 9, 0, 0, 0, 
+    1, 0, 0, 7, 0, 0, 0, 0, 6, 
+    0, 0, 0, 5, 8, 0, 9, 0, 0, 
+    7, 0, 5, 0, 0, 0, 8, 0, 4, 
+    0, 0, 9, 0, 6, 2, 0, 0, 0, 
+    9, 0, 0, 0, 0, 4, 0, 0, 1, 
+    0, 0, 0, 3, 0, 0, 4, 9, 0, 
+    4, 1, 0, 0, 0, 0, 2, 0, 8 
+    ]
+
+-- http://lipas.uwasa.fi/~timan/sudoku/
+testSudoku_s15c :: Puzzle
+testSudoku_s15c = [
+    7, 9, 0, 0, 0, 0, 0, 0, 3, 
+    0, 0, 0, 0, 0, 0, 0, 6, 0, 
+    8, 0, 1, 0, 0, 4, 0, 0, 2, 
+    0, 0, 5, 0, 0, 0, 0, 0, 0, 
+    3, 0, 0, 1, 0, 0, 0, 0, 0, 
+    0, 4, 0, 0, 0, 6, 2, 0, 9, 
+    2, 0, 0, 0, 3, 0, 0, 0, 6, 
+    0, 3, 0, 6, 0, 5, 4, 2, 1, 
+    0, 0, 0, 0, 0, 0, 0, 0, 0
+                  ]
